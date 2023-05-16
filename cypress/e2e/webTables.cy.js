d1131a8a3ee0a92df88c851088b85ecdc66419e8
AbdullahDemircan <abdullah.demircan.78@gmail.com>
2023-05-16 19:02:12 +0200

    add: newesplint1

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -28,6 +28,38 @@ describe('Cypress WebTable Tests', { baseUrl: 'https://demoqa.com'},() => {
             cy.wrap(row).find('.rt-td').eq(1).should('contain', 'Specter');
         })  
 
+        it('Check finding and deleting a record', () => {
+            cy.get('.rt-tbody')    
+            .contains('.rt-tr-group','Alden')
+            .then((row) => {
+                // click on Delete button for Alden record
+                cy.wrap(row).find('[title="Delete"]').click();
+            })
+            // Assert that table does NOT have Alden record
+            cy.get('.rt-tbody').should('not.contain', 'Alden');
+            // search for Alden in the body  
+            cy.get('#searchBox').type('Alden');
+            // Assert that there is no record
+            cy.get('.rt-tbody').should('not.contain', 'Alden');
+            // No data found element is visible or not
+            cy.get('.rt-noData').should('contain','No rows found')
+            .should('be.visible');
+         })
+
+         it('Search  for different age record', () => {
+            //define age groups
+            const ageGroup =[29,39,45,77];
+            // for each age group perform same test scenario
+            cy.wrap(ageGroup).each((age) => {
+                //type age into search box
+                cy.get('#searchBox').clear().type(age);
+
+
+                // verify if that age exist, number of records
+                cy.get('.rt-tbodt').find('.rt-tr-group').first().should('contain', age);
+                cy.get('-rt-tbody').contains('.rt-tr-group', age).should('have.length', 1);
+            })
+         })
      });
     
    });
